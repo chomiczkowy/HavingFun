7651fbf859fb0f5e655c24e2133b1ce6920516db
Karol Latka <karolas-borys@wp.pl>
2019-09-05 00:01:57 +0200

    added solution

---
+++ src/HavingFun/HavingFun.Web.MainSite/AppSettings/ConnectionStrings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HavingFun.Web.MainSite.AppSettings
+{
+    public class ConnectionStrings
+    {
+        public string MainDb { get; set; }
+    }
+}
---
+++ src/HavingFun/HavingFun.Web.MainSite/AppSettings/CustomSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HavingFun.Web.MainSite.AppSettings
+{
+    public class CustomSettings
+    {
+        public string MySetting { get; set; }
+    }
+}
